22678defda395bcdab95b2e08c272b3188b24a8f
iravinderr <imravinderr@gmail.com>
2024-12-02 11:06:43 +0530

    changes

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -28,7 +28,7 @@ const Post = () => {
         <p>Image Caption : It will be a very long text regarding the image</p>
         <div className="post-container rounded-xl mb-[0.8rem] mt-[0.8rem]">
           <img
-            src="../../public/kitty.jpg"
+            src={kitty}
             className=" post-image"
             alt="These are all the posts User has sent"
           />
