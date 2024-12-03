8fd9e6aba00f77279f357a852a0f6d483b4a268a
iravinderr <imravinderr@gmail.com>
2024-12-03 16:10:09 +0530

    changes

--- frontend/src/components/Middle.jsx
+++ frontend/src/components/Middle.jsx
@@ -1,19 +1,19 @@
-import React, { useEffect, useState } from 'react'
-import Post from './Post'
-import { getRequestAxios, postRequestAxios } from '../services/requests'
-import { createPostAPI, getPostsForHomeAPI } from '../services/apis'
-import toast from 'react-hot-toast'
-import Loader from './Loader'
-import { rawPostData } from '../assets/rawdata';
+import React, { useEffect, useState } from "react";
+import Post from "./Post";
+import { getRequestAxios, postRequestAxios } from "../services/requests";
+import { createPostAPI, getPostsForHomeAPI } from "../services/apis";
+import toast from "react-hot-toast";
+import Loader from "./Loader";
+import { rawPostData } from "../assets/rawdata";
 
 const Middle = () => {
-  const [showForm,setShowForm] = useState(false);
-  const [loading,setLoading] = useState(false);
+  const [showForm, setShowForm] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [posts, setPosts] = useState(rawPostData);
-  const [formData,setFormData] = useState({
-    tags:"",
-    caption:"",
-    images:[]
+  const [formData, setFormData] = useState({
+    tags: "",
+    caption: "",
+    images: [],
   });
 
   const handleInputChange = (e) => {
@@ -28,20 +28,24 @@ const Middle = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log("Submitting form data: ", formData);
-    
+
     setLoading(true);
     try {
-      const response = await postRequestAxios(createPostAPI, {
-        caption:formData.caption,
-        tags:formData.tags,
-        files:formData.images
-      }, null, null);
+      const response = await postRequestAxios(
+        createPostAPI,
+        {
+          caption: formData.caption,
+          tags: formData.tags,
+          files: formData.images,
+        },
+        null,
+        null
+      );
 
       if (response.data.success) {
-        
         setLoading(false);
-        setShowForm(false)
-        navigate("/profile")
+        setShowForm(false);
+        navigate("/profile");
         toast.success(response.data.message);
       }
     } catch (error) {
@@ -49,17 +53,19 @@ const Middle = () => {
       toast.error(error.response.data.message);
     }
     setFormData({
-      tags:"",
+      tags: "",
       caption: "",
-      images: []
+      images: [],
     });
   };
-  
 
-  (async () => {
+  async () => {
     setLoading(true);
     try {
-      const response = await getRequestAxios(getPostsForHomeAPI, { scrollCount: 1, postLimit: 10});
+      const response = await getRequestAxios(getPostsForHomeAPI, {
+        scrollCount: 1,
+        postLimit: 10,
+      });
       if (response.data.success) {
         setLoading(false);
         setPosts(response.data.data);
@@ -68,27 +74,33 @@ const Middle = () => {
       setLoading(false);
       toast.error(error.response.data.message);
     }
-  }, []);
+  },
+    [];
 
-  if(loading){
-    return <Loader />
+  if (loading) {
+    return <Loader />;
   }
 
   return (
-    <div className='flex flex-col'>
-        <div className='bg-white flex justify-end items-center w-[56.5vw] h-[4rem] shadow-md fixed'>
-            <button onClick={() => setShowForm(true)} className=' bg-[#6366f1] w-[10rem] h-[2.5rem] mr-[1rem] text-white rounded-3xl hover:bg-[#4f52db]'>Add New Post +</button>
-        </div>
+    <div className="flex flex-col">
+      <div className="bg-white flex justify-end items-center w-[56.5vw] h-[4rem] shadow-md fixed">
+        <button
+          onClick={() => setShowForm(true)}
+          className=" bg-[#6366f1] w-[10rem] h-[2.5rem] mr-[1rem] text-white rounded-3xl hover:bg-[#4f52db]"
+        >
+          Add New Post +
+        </button>
+      </div>
 
-        <div className='posts w-[56.5vw] mt-[2rem] pt-[8vh]'>
-          {posts.map((postData) => (
-              <Post postData={postData}/>
-            ))}
-        </div>
+      <div className="posts w-[56.5vw] mt-[2rem] pt-[8vh]">
+        {posts.map((postData) => (
+          <Post postData={postData} />
+        ))}
+      </div>
 
-        {showForm && (
+      {showForm && (
         <div
-        onClick={() => setShowForm(false)}
+          onClick={() => setShowForm(false)}
           style={{
             position: "fixed",
             top: "0",
@@ -103,7 +115,7 @@ const Middle = () => {
           }}
         >
           <div
-          onClick={(e) => e.stopPropagation()}
+            onClick={(e) => e.stopPropagation()}
             style={{
               backgroundColor: "#fff",
               padding: "20px",
@@ -112,12 +124,10 @@ const Middle = () => {
               boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
             }}
           >
-            <h2 className='text-2xl pb-[1rem]'>Add a New Post</h2>
+            <h2 className="text-2xl pb-[1rem]">Add a New Post</h2>
             <form onSubmit={handleSubmit}>
-              
-              
               <div style={{ marginBottom: "10px" }}>
-                <label className='text-lg'>Caption : </label>
+                <label className="text-lg">Caption : </label>
                 <textarea
                   name="caption"
                   value={formData.caption}
@@ -127,10 +137,10 @@ const Middle = () => {
                 ></textarea>
               </div>
               <div style={{ marginBottom: "10px" }}>
-                <label className='text-lg'>Tags(comma-seperated) : </label>
+                <label className="text-lg">Tags(comma-seperated) : </label>
                 <input
                   name="tags"
-                  type='text'
+                  type="text"
                   value={formData.tags}
                   onChange={handleInputChange}
                   required
@@ -181,7 +191,7 @@ const Middle = () => {
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default Middle
+export default Middle;
