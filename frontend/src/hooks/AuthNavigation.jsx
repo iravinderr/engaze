25cd25f6459fc2807d60f1a69e1cf69989cbf7c2
iravinderr <imravinderr@gmail.com>
2024-12-03 16:41:04 +0530

    auth navigation handled

--- frontend/src/hooks/AuthNavigation.jsx
+++ frontend/src/hooks/AuthNavigation.jsx
@@ -1,16 +1,25 @@
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+
 
 const useAuthNavigation = () => {
   const navigate = useNavigate();
+  const location = useLocation();
   const { loading, setLoading, authenticated, setAuthenticated } = useAuth();
 
   useEffect(() => {
-    if (!authenticated) {
-      navigate("/");
+    if (!loading) {
+      // If the user is authenticated and tries to go to the login or signup page, redirect them to a default page like /home
+      if (authenticated && (location.pathname === "/" || location.pathname === "/signup")) {
+        navigate("/home", { replace: true });  // Or you can redirect to /feed or any other page
+      }
+      // If the user is not authenticated and tries to access protected pages, redirect them to login
+      else if (!authenticated && location.pathname !== "/" && location.pathname !== "/signup") {
+        navigate("/", { replace: true }); // Redirect to login if not authenticated
+      }
     }
-  }, [authenticated, navigate]);
+  }, [authenticated, loading, location, navigate]);
 
   return { authenticated, setAuthenticated, loading, setLoading };
 };
