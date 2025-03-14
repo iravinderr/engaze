ddca01fa159adf092f734490ebc98d4237a94ddf
iravinderr <imravinderr@gmail.com>
2025-03-14 15:32:35 +0530

    zod validation updated for username

--- backend/src/utils/zod.utils.js
+++ backend/src/utils/zod.utils.js
@@ -8,7 +8,8 @@ const registrationSchema = z.object({
         
     username: z.string()
         .min(3, { message: "Username must be at least 3 characters long." })
-        .max(50, { message: "Username cannot exceed 50 characters." }),
+        .max(50, { message: "Username cannot exceed 50 characters." })
+        .regex(/^[a-zA-Z0-9_.]+$/, { message: "Username can only contain letters, numbers, '_' and '.'" }),
 
     email: z.string()
         .email({ message: "Invalid email format." })
