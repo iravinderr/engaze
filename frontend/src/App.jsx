16b217189720d4679233e4298b9df2ff8e6c21d3
Arush Kumar <arush.beast012@gmail.com>
2025-05-25 22:12:45 +0530

    connect wallet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { BottomNav, SideBar } from "./components";
 import { Outlet } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import useAuthNavigation from "./hooks/AuthNavigation";
 
+
 function App() {
   const { authenticated } = useAuthNavigation();
 
+
+
   return (
-      <div className="h-screen w-screen flex">
-        <div className="hidden w-[17vw] md:block">{!authenticated ? null : <SideBar />}</div>
-        <div><Outlet /></div>
-        <Toaster />
-        <div className="md:hidden">{!authenticated ? null : <BottomNav />}</div>
+
+    
+    <div className="h-screen w-screen flex">
+      <div className="hidden w-[17vw] md:block">
+        {!authenticated ? null : <SideBar />}
       </div>
+      <div>
+        <Outlet />
+      </div>
+      <Toaster />
+      <div className="md:hidden">
+        {!authenticated ? null : <BottomNav />}
+      </div>{" "}
+    </div>
   );
 }
 
